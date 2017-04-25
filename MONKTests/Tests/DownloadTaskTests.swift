95dd3c3ed2c0a9f2a208b6ba10e6eae965fb79f7
Andrew Mayers <andrew@mayers.us>
2017-04-25 12:08:26 -0400

    ensure download tasks aren’t cached

--- MONKTests/Tests/DownloadTaskTests.swift
+++ MONKTests/Tests/DownloadTaskTests.swift
@@ -12,10 +12,17 @@ import XCTest
 class DownloadTaskTests: XCTestCase {
     
     private let networkController = NetworkController(serverTrustSettings: nil)
-    
+    private let requestedLocalURL = URL(fileURLWithPath: NSTemporaryDirectory() + "image.jpg")
+
+    override func setUp() {
+        super.setUp()
+        try? FileManager.default.removeItem(at: requestedLocalURL)
+    }
+
     override func tearDown() {
         super.tearDown()
         networkController.cancelAllTasks()
+        try? FileManager.default.removeItem(at: requestedLocalURL)
     }
     
     func testDownloadTaskProgress() {
@@ -74,7 +81,6 @@ class DownloadTaskTests: XCTestCase {
     
     func testDownloadTaskCancellation() {
         let url = URL(string: "https://httpbin.org/image/jpeg")!
-        let requestedLocalURL = URL(fileURLWithPath: NSTemporaryDirectory() + "image.jpg")
         let dummyFileURL = DataHelper.imageURL(for: .compiling)
         try! FileManager.default.copyItem(at: dummyFileURL, to: requestedLocalURL)
         
